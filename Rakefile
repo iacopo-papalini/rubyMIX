c0e41a0ebcda512aea37380dfb2f2731adac563b
Iacopo Papalini <iacopo.papalini@register.it>
2013-10-19 22:46:34 +0200

    let's try with simplecov

--- Rakefile
+++ Rakefile
@@ -1,5 +1,5 @@
-require 'spec_helper'
 require 'rspec/core/rake_task'
+
 RSpec::Core::RakeTask.new(:spec) do |t|
   t.rspec_opts = '-P src/'
 
--- spec_helper.rb
+++ spec_helper.rb
@@ -3,4 +3,6 @@ require 'simplecov-gem-adapter'
 SimpleCov.start 'gem' do
   @filters = []
   add_filter('spec/')
+  add_filter('/var/lib')
+
 end
