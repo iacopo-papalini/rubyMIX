6b7226ecf1aaf1dcba3ddedffaa2c3f7325e9014
Iacopo Papalini <iacopo.papalini@register.it>
2013-11-16 16:32:43 +0100

    refactoring

--- src/assembler/assembly_descriptor_generator.rb
+++ src/assembler/assembly_descriptor_generator.rb
@@ -1,6 +1,8 @@
 require 'yaml'
 
 class AssemblyDescriptorGenerator
+  INDENT = "\t"
+
   def initialize(file)
     @file = file
   end
@@ -8,41 +10,62 @@ class AssemblyDescriptorGenerator
   def generate
     data = YAML.load_file(@file)
     string = ''
-    data.each do |key, value|
-      string << "class %s\n\tOPERATION = {}\n\tINSTRUCTION = {}\n\tF_VALUE = {}\n\tF_STR = {}\n" % key
-      operations = ''
-      f_values = ''
-
-      indent = "\t"
-      value.each_with_index do |code, opCode|
-
-        if code.is_a?(Array) then
-          str_initialized = false
-          code.each_with_index do |realCode, f|
-            next if realCode == nil
-
-            operations << indent << ("OP_%s = %d\n" % [realCode, opCode])
-            operations << indent << ("OPERATION['%s'] = OP_%s\n" % [realCode, realCode])
-            operations << indent << ("INSTRUCTION[OP_%s] = '%s'\n" % [realCode, realCode])
-            f_values << indent << ("F_%s = %d\n" % [realCode, f])
-            f_values << indent << ("F_STR[OP_%s] = {}\n" % realCode) if not str_initialized
-            str_initialized = true
-            f_values << indent << ("F_VALUE['%s'] = F_%s\n" % [realCode, realCode])
-            f_values << indent << ("F_STR[OP_%s][F_%s] = '%s'\n" % [realCode, realCode, realCode])
-
-          end
-        else
-          operations << indent << ("OP_%s = %d\n" % [code, opCode])
-          operations << indent << ("OPERATION['%s'] = OP_%s\n" % [code, code])
-          operations << indent << ("INSTRUCTION[OP_%s] = '%s'\n" % [code, code])
-        end
-      end
-
-      string << operations
-      string << f_values
-      string << "end\n"
+    data.each do |class_name, constants_list|
+      string << generate_class(class_name, constants_list)
     end
     string
   end
 
+  def generate_class(class_name, constants_list)
+    string = ''
+    print "Generating class %s\n" %class_name
+    string << "class %s\n\tOPERATION = {}\n\tINSTRUCTION = {}\n\tF_VALUE = {}\n\tF_STR = {}\n" % class_name
+    acc_operations = ''
+    acc_f_values = ''
+
+    constants_list.each_with_index do |code, op_code|
+      operations, f_values = generate_code(code, op_code)
+      print "Generated code for %s constant: (%d characters)\n" % [code, operations.length + f_values.length]
+      acc_operations << operations
+      acc_f_values << f_values
+    end
+
+    string << acc_operations
+    string << acc_f_values
+    string << "end\n"
+  end
+
+  def generate_code(code, op_code)
+    if code.is_a?(Array)
+      multi_instructions_op_code(op_code, code)
+    else
+      [create_operation_constants(op_code, code), '']
+    end
+  end
+
+  def multi_instructions_op_code(op_code, code)
+    str_initialized = false
+    operations = ''
+    f_values = ''
+    code.each_with_index do |realCode, f|
+      next if realCode == nil
+      operations << create_operation_constants(op_code, realCode)
+      f_values << INDENT << ("F_STR[OP_%s] = {}\n" % realCode) if not str_initialized
+      f_values << create_f_field_constants(f, realCode)
+      str_initialized = true
+    end
+    [operations, f_values]
+  end
+
+  def create_f_field_constants(f, real_code)
+    INDENT + ("F_%s = %d\n" % [real_code, f]) +
+        INDENT + ("F_VALUE['%s'] = F_%s\n" % [real_code, real_code]) +
+        INDENT + ("F_STR[OP_%s][F_%s] = '%s'\n" % [real_code, real_code, real_code])
+  end
+
+  def create_operation_constants(op_code, real_code)
+    INDENT + ("OP_%s = %d\n" % [real_code, op_code]) +
+        INDENT + ("OPERATION['%s'] = OP_%s\n" % [real_code, real_code]) +
+        INDENT + ("INSTRUCTION[OP_%s] = '%s'\n" % [real_code, real_code])
+  end
 end
